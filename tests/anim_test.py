bf0d246bd0cada5504658581e1498eb92d8d75b9
Stephen Lennon <salennon@hotmail.co.uk>
2022-02-27 09:03:46 +0000

    Updated animation test anim_test.py

--- tests/anim_test.py
+++ tests/anim_test.py
@@ -7,17 +7,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-fig, ax = plt.subplots()
-time_points = np.arange(0, 10, 0.25)
-im = ax.imshow(np.random.rand(10,10))
 
-def generate_frame(i):
-    field = np.random.rand(10,10)
-    im.set_array(field)
-    return [im]        #FuncAnimation requires iterable returned
+def main():
+    '''
+    Test matplotlib animation with random data
+    '''
+    fig, ax = plt.subplots()
+    time_points = np.arange(0, 10, 0.25)
+    im = ax.imshow(np.random.rand(10,10))
 
-anim = FuncAnimation(fig, generate_frame,
-                        frames = len(time_points))
-                    
-dir_path = os.path.dirname(os.path.realpath(__file__))
-anim.save(f'{dir_path}/test_output/test.gif')
+    def generate_frame(i):
+        field = np.random.rand(10,10)
+        im.set_array(field)
+        return [im]        #FuncAnimation requires iterable returned
+
+    anim = FuncAnimation(fig, generate_frame,
+                            frames = len(time_points))
+                        
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    anim.save(f'{dir_path}/test_output/test.gif')
+
+
+if __name__ == "__main__":
+    main()
